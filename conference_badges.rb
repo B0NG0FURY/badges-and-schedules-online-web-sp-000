db61463d4ce757be1ccb5e8ebf205fbed7369f51
John Troutman <jtroutmanman@gmail.com>
2020-09-17 17:47:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,3 +5,11 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
+def batch_badge_creator(names)
+  badges = []
+  names.each do |name|
+    badges << badge_maker(name)
+  end
+  badges
+end
+
